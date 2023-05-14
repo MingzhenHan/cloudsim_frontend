0cb8af647d4a570dd5a2c3ba35540a1bc225a1bb
MingzhenHan <hanmingzhen2002@outlook.com>
2023-05-14 17:50:43 +0800

    add upload file function

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+<el-upload
+  class="upload-demo"
+  action="http://localhost:8080/uploadfile"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :before-remove="beforeRemove"
+  multiple
+  :limit="3"
+  :on-exceed="handleExceed"
+  :file-list="fileList">
+  <el-button size="small" type="primary">上传任务配置文件</el-button>
+</el-upload>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        fileList: []
+      };
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
