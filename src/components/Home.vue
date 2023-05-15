581e524dc66b450b7f75d87a1b7e11698eab93bc
MingzhenHan <hanmingzhen2002@outlook.com>
2023-05-15 22:53:31 +0800

    show simulation result flowchart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,216 +6,44 @@
   :on-preview="handlePreview"
   :on-remove="handleRemove"
   :before-remove="beforeRemove"
+  :on-success="saveName"
   multiple
   :limit="3"
   :on-exceed="handleExceed"
   :file-list="fileList">
   <el-button size="small" type="primary">上传任务配置文件</el-button>
 </el-upload>
+
+<el-radio-group v-model="radio">
+    <el-radio :label="1">MAXMIN</el-radio>
+    <el-radio :label="2">MINMIN</el-radio>
+    <el-radio :label="3">MCT</el-radio>
+    <el-radio :label="4">DATA</el-radio>
+    <el-radio :label="5">STATIC</el-radio>
+    <el-radio :label="6">FCFS</el-radio>
+    <el-radio :label="7">ROUNDROBIN</el-radio>
+</el-radio-group>
+<br>
+<el-button class="button" type="success" round @click="getGraph">Start Cloud Simulation</el-button>
 <div id="container"></div>
 </div>
 </template>
 
 <script>
 import G6 from '@antv/g6';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:8080';
 export default {
     data() {
       return {
         fileList: [],
-        newdata:{
-          nodes: [
-    {
-      id: '1',
-      dataType: 'alps',
-      name: 'alps_file1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '2',
-      dataType: 'alps',
-      name: 'alps_file2',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '3',
-      dataType: 'alps',
-      name: 'alps_file3',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '4',
-      dataType: 'sql',
-      name: 'sql_file1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '5',
-      dataType: 'sql',
-      name: 'sql_file2',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '6',
-      dataType: 'feature_etl',
-      name: 'feature_etl_1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '7',
-      dataType: 'feature_etl',
-      name: 'feature_etl_1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '8',
-      dataType: 'feature_extractor',
-      name: 'feature_extractor',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-  ],
-  edges: [
-    {
-      source: '1',
-      target: '2',
-    },
-    {
-      source: '1',
-      target: '3',
-    },
-    {
-      source: '2',
-      target: '4',
-    },
-    {
-      source: '3',
-      target: '4',
-    },
-    {
-      source: '4',
-      target: '5',
-    },
-    {
-      source: '5',
-      target: '6',
-    },
-    {
-      source: '6',
-      target: '7',
-    },
-    {
-      source: '6',
-      target: '8',
-    },
-  ]
-        }
+        filename:"",
+        radio:1,
+        newdata:{}
       };
     },
     methods: {
-      handleRemove(file, fileList) {
+      handleRemove(file, fileList) { 
         console.log(file, fileList);
       },
       handlePreview(file) {
@@ -226,6 +54,23 @@ export default {
       },
       beforeRemove(file, fileList) {
         return this.$confirm(`确定移除 ${ file.name }？`);
+      },
+      saveName(response, file, fileList){
+        this.filename=file.name;
+      },
+      async getGraph(){
+        console.log(this.filename);
+        var url= '/getGraph?name='+ this.filename +"&algorithm="+this.radio;
+        console.log(url);
+        await axios.get(url)
+          .then(response => {
+          this.newdata=response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+        console.log(this.newdata)
+        this.initG6();
       },
       initG6(){
         G6.registerNode(
@@ -318,12 +163,14 @@ const graph = new G6.Graph({
           });
           return text.join('\n');
         },
-        offset: 30,
+        offset: 5
       },
     ],
   },
   fitView: true,
 });
+console.log("qing");
+console.log(this.newdata);
 graph.data(this.newdata);
 graph.render();
 
@@ -334,14 +181,27 @@ if (typeof window !== 'undefined')
     graph.changeSize(container.scrollWidth, container.scrollHeight);
   };
       }
-    },
-    mounted()
-    {
-      this.initG6();
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.upload-demo {
+  margin-bottom: 10px;
+}
+.button{
+  margin-top: 10px;
+}
+#container {
+  position: relative;
+}
+.g6-tooltip {
+    border-radius: 6px;
+    font-size: 12px;
+    color: #fff;
+    background-color: #000;
+    padding: 2px 8px;
+    text-align: center;
+}
 </style>
